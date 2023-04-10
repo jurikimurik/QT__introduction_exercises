c3803582e3396eebc6f70e9752c36fe348fb3ba1
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-10 23:10:45 +0200

    4.4 Exercise 1: classes are now fixed and cut in some places (according to UML diagram)

--- 4.4/1/person.cpp
+++ 4.4/1/person.cpp
@@ -1,4 +1,9 @@
 #include "person.h"
 
 Person::Person(QString name, bool isEmloyed, Employer* employer) :
-    m_Name(name), m_Employed(isEmloyed), m_Employer(employer) {}
+    m_Name(name), m_Employed(isEmloyed) {}
+
+
+QString Person::getName() {
+    return m_Name;
+}
--- 4.4/1/person.h
+++ 4.4/1/person.h
@@ -12,13 +12,9 @@ class Person
 private:
     QString m_Name;
     bool m_Employed;
-    Employer* m_Employer;
-    Position m_Position;
 public:
     Person(QString name, bool isEmloyed = false, Employer* employer = nullptr);
 
-    Position getPosition();
-    Employer getEmployer();
     QString getName();
 };
 
