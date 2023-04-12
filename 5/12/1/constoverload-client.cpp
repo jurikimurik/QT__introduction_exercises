3f424ce3bd0d224145abeb821264877edcc80a76
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-12 16:49:58 +0200

    5.12 Exercise 2 is now completed.

--- 5/12/1/constoverload-client.cpp
+++ 5/12/1/constoverload-client.cpp
@@ -1,9 +1,13 @@
 #include "point3.h"
 #include <iostream>
+#include <random>
+#include <chrono>
+using namespace std;
+
+int myRand(int, int);
 
 int main()
 {
-    using namespace std;
     Point3 pt1(1.2, 3.4, 5.6);
     const Point3 pt2(7.8, 9.1, 6.4);
     double d;
@@ -24,10 +28,22 @@ int main()
     cout << (pt1+pt2) << endl;
     cout << (pt1-pt2) << endl;
     cout << (pt1 * 9.4) << endl;
+
+    cout << myRand(1, 20) << myRand(1, 20) << endl;
 }
 
 
 std::ostream& operator<< (std::ostream& out, const Point3& v)
 {
     return out << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]";
+}
+
+
+int myRand(int min, int max)
+{
+    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+    default_random_engine dre(seed);
+    uniform_int_distribution id(min, max);
+
+    return id(dre);
 }
