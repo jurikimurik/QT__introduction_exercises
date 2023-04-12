bc3bf744e56f819d76b609c0aeb2ee783804487b
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-12 16:55:10 +0200

    5.12 Exercise 3 is now completed.

--- 5/12/1/constoverload-client.cpp
+++ 5/12/1/constoverload-client.cpp
@@ -2,9 +2,12 @@
 #include <iostream>
 #include <random>
 #include <chrono>
+#include <algorithm>
+#include <QVector>
 using namespace std;
 
 int myRand(int, int);
+QVector<int> randomPerm(int n, unsigned key);
 
 int main()
 {
@@ -30,6 +33,11 @@ int main()
     cout << (pt1 * 9.4) << endl;
 
     cout << myRand(1, 20) << myRand(1, 20) << endl;
+
+    for(const auto& elem : randomPerm(10, 1001))
+    {
+        cout << elem << ", ";
+    }
 }
 
 
@@ -38,12 +46,27 @@ std::ostream& operator<< (std::ostream& out, const Point3& v)
     return out << "[" << v[0] << ", " << v[1] << ", " << v[2] << "]";
 }
 
+QVector<int> randomPerm(int n, unsigned key)
+{
+    default_random_engine dre(key);
+
+
+    QVector<int> wektor;
+    while(n-- > 0)
+    {
+        wektor.push_front(n);
+    }
+
+    shuffle(wektor.begin(), wektor.end(), dre);
+    return wektor;
+}
+
 
 int myRand(int min, int max)
 {
     auto seed = std::chrono::system_clock::now().time_since_epoch().count();
     default_random_engine dre(seed);
-    uniform_int_distribution id(min, max);
+    uniform_int_distribution id(min, max-1);
 
     return id(dre);
 }
