70777acd8cb1a00a4aa2ee585a99d987730a42df
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-13 18:16:21 +0200

    6.1.2 point 3 is now complete.

--- 6/1.2/student-test.cpp
+++ 6/1.2/student-test.cpp
@@ -17,6 +17,16 @@ void finish(Student* student) {
          << student->getClassName()
          << " wnioskuje o tytul.\n"
          << student->toString() << "\n";
+
+    std::string str = student->toString().toStdString();
+
+    auto pocz = str.find("rok: ") + 4;
+    auto kon = str.find(";", pocz);
+
+    int lata = std::stoi(str.substr(pocz, kon));
+    if(lata != 4 && lata != 6)
+        tekst += "\nPanie, zwariowales! Nie mozesz jeszcze wnioskowac o tytul!";
+
     wypisz_w_oknie(tekst);
 }
 
@@ -28,11 +38,14 @@ int main(int argc, char* argv[])
 
     Undergrad us("Frodo Baggins", 5562, "Teoria Pierscieni", 4, 1220);
     GradStudent gs("Bilbo Baggins", 3029, "Historia", 6, GradStudent::fellowship);
+    Undergrad ns("Ricardo Milas", 2342, "Wychowanie fizyczne", 2, 100);
     cout << "Oto dane naszych dwoch studentow:\n";
     wypisz_w_oknie(gs.toString());
     wypisz_w_oknie(us.toString());
+    wypisz_w_oknie(ns.toString());
     cout << "\nOto co sie stanie, gdy obaj skoncza studia:\n";
     finish(&us);
     finish(&gs);
+    finish(&ns);
     return 0;
 }
