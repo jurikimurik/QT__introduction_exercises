1ebfc55d0c169aa93bce3383416b55e0045ea3bb
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-13 18:05:36 +0200

    6.1.2 point 2 is now complete.

--- 6/1.2/student-test.cpp
+++ 6/1.2/student-test.cpp
@@ -1,23 +1,36 @@
 #include <QTextStream>
+#include <QtWidgets>
 #include "student.h"
 using namespace Qt;
 
 static QTextStream cout(stdout);
 
+void wypisz_w_oknie(QString nadpis)
+{
+    QMessageBox::information(nullptr, "Okienko", nadpis, "Ok");
+}
+
 void finish(Student* student) {
-    cout << "\nNastepujacy "
+    QString tekst;
+    QTextStream os(&tekst);
+    os << "\nNastepujacy "
          << student->getClassName()
          << " wnioskuje o tytul.\n"
          << student->toString() << "\n";
+    wypisz_w_oknie(tekst);
 }
 
-int main()
+
+
+int main(int argc, char* argv[])
 {
+    QApplication app(argc, argv);
+
     Undergrad us("Frodo Baggins", 5562, "Teoria Pierscieni", 4, 1220);
     GradStudent gs("Bilbo Baggins", 3029, "Historia", 6, GradStudent::fellowship);
     cout << "Oto dane naszych dwoch studentow:\n";
-    cout << gs.toString() << endl;
-    cout << us.toString() << endl;
+    wypisz_w_oknie(gs.toString());
+    wypisz_w_oknie(us.toString());
     cout << "\nOto co sie stanie, gdy obaj skoncza studia:\n";
     finish(&us);
     finish(&gs);
