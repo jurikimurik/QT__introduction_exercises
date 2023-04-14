554a425917f6ca46d2439c5f781cc2256f5f4325
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-14 17:58:52 +0200

    6.9.1 is now complete.

--- 6/9.1/card-tester.cpp
+++ 6/9.1/card-tester.cpp
@@ -1,29 +1,32 @@
 #include "carddeck.h"
 #include <QTextStream>
+#include <QtGui>
+#include <QtWidgets>
 using namespace Qt;
 
-QTextStream cout(stdout);
-
-int main()
+int main(int argc, char* argv[])
 {
-    CardDeck talia;
-
-
-    auto gracz1 = talia.deal(15);
-    auto gracz2 = talia.deal(15);
-    auto gracz3 = talia.deal(15);
-    auto gracz4 = talia.deal(15);
-    auto gracz5 = talia.deal(15);
-
-    cout << "Karty w talii: " << talia.toString() << endl;
-
-    cout << "Karty gracza 1: " << gracz1.toString() << endl;
-    cout << "Wartosc kart gracza 1: " << gracz1.getValue() << endl;
-    cout << "Karty gracza 2: " << gracz2.toString() << endl;
-    cout << "Karty gracza 3: " << gracz3.toString() << endl;
-    cout << "Karty gracza 4: " << gracz4.toString() << endl;
-    cout << "Karty gracza 5: " << gracz5.toString() << endl;
-
-
-
+    QApplication app(argc, argv);
+    QTextStream cout(stdout);
+    int handSize, playerScore, progScore;
+    cout << "Ile kart dla jednego gracza? " << flush;
+    handSize = QInputDialog::getInt(0, QString("getInt()"), QString("Ile kart na rece? "), 1, 5);
+    QMessageBox::StandardButton sb;
+    do {
+        CardDeck deck;
+        CardHand hand;
+        hand = deck.deal(handSize);
+        cout << "Oto twoje karty: " << endl;
+        cout << hand.toString() << endl;
+        playerScore = hand.getValue();
+        cout << QString("Twoj wynik to: %1 punktow.").arg(playerScore) << endl;
+        // teraz karty dla rozdajacego
+        hand = deck.deal(handSize);
+        progScore = hand.getValue();
+        cout << "Oto moje karty: " << endl;
+        cout << hand.toString() << endl;
+        cout << QString("Moj wynik to: %1 punktow.").arg(progScore) << endl;
+        cout << QString("%1 wygrywa!!!").arg((playerScore > progScore) ? "Ty" : "Ja") << endl;
+        sb = QMessageBox::question(0, QString("QMessageBox::question()"), QString("Kolejne rozdanie?"), QMessageBox::Yes | QMessageBox::No);
+    } while(sb == QMessageBox::Yes);
 }
