cb5cb8ca6dbcf8e91fcb1566f9f3c62339fc5dad
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-19 17:47:24 +0200

    8.2.2.1 point 3 is now completed.

--- 8/2.2/bunch.cpp
+++ 8/2.2/bunch.cpp
@@ -4,6 +4,17 @@ using namespace Qt;
 
 QTextStream cout(stdout);
 
+void showTree(QObject* theparent)
+{
+    auto lista = theparent->children();
+    for(auto& elem : lista)
+    {
+        cout << "Skladowa: " << elem->objectName() << " - Rodzic: " << elem->parent()->objectName() << endl;
+        if(elem->children().size() > 0)
+            showTree(elem);
+    }
+}
+
 // wyhoduj fromadke
 void growBunch() {
     qDebug() << "Stworzmy najpierw gromade obiektow." << endl;
@@ -19,6 +30,11 @@ void growBunch() {
     new Person("Jan", carol);
     new Person("Cindy", carol);
     new Person("Alice");
+
+    showTree(&bunch);
+
+
+
     qDebug() << "\nWyswietl liste za pomoca QObject::dumpObjectTree()" << endl;
     bunch.dumpObjectTree();
     cout << "\nMozemy zwrocic wartosci z growBunch() - " << " Zniszczenie wszystkich "
