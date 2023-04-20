a142e7ce2c26b579fc1b6f91f223bd66993524ad
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-20 15:05:17 +0200

    9.6.3 point 2: puzzlemodel class is now finally ready

--- 9/6.3/2/puzzlemodel.cpp
+++ 9/6.3/2/puzzlemodel.cpp
@@ -4,19 +4,6 @@
 #include <algorithm>
 
 #include <QDebug>
-
-PuzzleModel::PuzzleModel(QObject* parent, int rows, int cols) : QObject(parent), m_Rows(rows), m_Cols(cols)
-{
-    for(int i = 0; i < m_Rows*m_Cols; ++i)
-    {
-        m_Positions.push_back(i+1);
-    }
-
-    //std::default_random_engine dre(std::chrono::system_clock::now().time_since_epoch().count());
-    std::default_random_engine dre(288);
-    std::shuffle(m_Positions.begin(), m_Positions.end(), dre);
-}
-
 void wypisz(QString co, bool czy_koniec = true)
 {
     qDebug() << co;
@@ -25,10 +12,23 @@ void wypisz(QString co, bool czy_koniec = true)
     qDebug() << Qt::flush;
 }
 
+
+PuzzleModel::PuzzleModel(QObject* parent, int rows, int cols) : QObject(parent), m_Rows(rows), m_Cols(cols)
+{
+    for(int i = 0; i < m_Rows*m_Cols; ++i)
+    {
+        m_Positions.push_back(i+1);
+    }
+
+    std::default_random_engine dre(std::chrono::system_clock::now().time_since_epoch().count());
+    std::shuffle(m_Positions.begin(), m_Positions.end(), dre);
+}
+
+
+
 int PuzzleModel::value(int r, int c)
 {
     int index = (r-1)*m_Cols + (c-1);
-    wypisz(QString("Indeks: ") + QString::number(index));
     if(index >= 0 && index < m_Positions.size())
     {
         return m_Positions.at(index);
@@ -56,31 +56,21 @@ bool PuzzleModel::slide(int tilenum)
     int with_r = r;
     int with_c = c;
 
-    wypisz(QString::number(r) + QString(" ") + QString::number(c));
-
-
     if(!neighboring(r+1, c)) {
-        wypisz("U dolu");
         with_r = r+1;
     } else if (!neighboring(r-1, c)) {
-        wypisz("U gory");
         with_r = r-1;
     } else if (!neighboring(r, c+1)) {
-        wypisz("Po prawej");
         with_c = c+1;
     } else if (!neighboring(r, c-1)) {
-        wypisz("Po lewej");
         with_c = c-1;
     }
 
-    if(r == with_r && c == with_c) {
+    if((r == with_r && c == with_c) || (with_r == 0 || with_c == 0)) {
         return false;
     }
 
-    qDebug() << r << " " << c << " " << with_r << " " << with_c << " " << Qt::flush << Qt::endl;
-
     int position_to = m_Positions.indexOf(value(with_r, with_c));
-    qDebug() << position << " " << position_to << Qt::flush;
     m_Positions.swapItemsAt(position, position_to);
     return true;
 }
