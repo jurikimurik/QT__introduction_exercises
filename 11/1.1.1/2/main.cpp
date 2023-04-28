9ff43e3c84c066df4565c30312981812b7a078da
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-28 07:57:37 +0200

    11.1.1.1: point 4 is now ready.

--- 11/1.1.1/2/main.cpp
+++ 11/1.1.1/2/main.cpp
@@ -3,7 +3,8 @@
 #include <QWidget>
 
 template <class T>
-void swap(T& a, T& b) {
+void super_swap(T& a, T& b) {
+    static_assert(std::is_copy_constructible<T>(), "Musi byc udostepniony operator przypisania");
     T temp = a;
     a = b;
     b = temp;
@@ -14,24 +15,30 @@ int main()
     int a = 10;
     int b = 25;
     qDebug() << a << " " << b;
-    swap(a, b);
+    super_swap(a, b);
     qDebug() << a << " " << b;
 
     QString tekst1 = "hello";
     QString tekst2 = "goodbye";
     qDebug() << tekst1 << " " << tekst2;
-    swap(tekst1, tekst2);
+    super_swap(tekst1, tekst2);
     qDebug() << tekst1 << " " << tekst2;
 
     int *toA = &a;
     int *toB = &b;
     qDebug() << *toA << " " << *toB;
-    swap(toA, toB);
+    super_swap(toA, toB);
     qDebug() << *toA << " " << *toB;
 
-    QWidget* object1 = new QWidget();
-    QWidget* object2 = new QWidget();
+    QObject* object1 = new QObject();
+    QObject* object2 = new QObject();
     qDebug() << object1 << " " << object2;
-    //swap(object1, object2);
+    super_swap(object1, object2);
     qDebug() << object1 << " " << object2;
+
+    QObject object3;
+    QObject object4;
+    qDebug() << &object3 << " " << &object4;
+    super_swap(object3, object4);
+    qDebug() << &object3 << " " << &object4;
 }
