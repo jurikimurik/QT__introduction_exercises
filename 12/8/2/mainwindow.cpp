1b22bdc0670fbec4856bd81c496052b6e707157b
Jurikimurik <makovskyiyurii@gmail.com>
2023-04-29 10:55:22 +0200

    12.8.2 is now ready.

--- 12/8/2/mainwindow.cpp
+++ 12/8/2/mainwindow.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QMetaObject>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QMetaObject::invokeMethod(this, "show");
 }
 
 MainWindow::~MainWindow()
