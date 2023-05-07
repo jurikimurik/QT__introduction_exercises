de918b2c416429290f911af9ab2ac32e00a71f4d
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-07 13:52:26 +0200

    14.6.1: Labels, edits, buttons connected to variables in HEADER file.

--- 14/6/1/addresswindow.cpp
+++ 14/6/1/addresswindow.cpp
@@ -6,6 +6,44 @@ AddressWindow::AddressWindow(QWidget *parent) :
     ui(new Ui::AddressWindow)
 {
     ui->setupUi(this);
+
+    //Dla make_pair
+    using namespace std;
+
+    //Inicjalizacja zmiennych
+    //  - COUNTRY
+    QComboBox* m_countryBox = ui->infoWidget->findChild<QComboBox*>("m_countryBox");
+    QLabel* m_countryLabel =  ui->infoWidget->findChild<QLabel*>("m_countryLabel");
+    m_countryPair = make_pair(m_countryLabel, m_countryBox);
+    //  - ADDRESS
+    QLabel* m_addressLabel = ui->infoWidget->findChild<QLabel*>("m_addressLabel");
+    QLineEdit* m_addressEdit =  ui->infoWidget->findChild<QLineEdit*>("m_addressEdit");
+    m_addressPair = make_pair(m_addressLabel, m_addressEdit);
+    //  - CITY
+    QLabel* m_cityLabel = ui->infoWidget->findChild<QLabel*>("m_cityLabel");
+    QLineEdit* m_cityEdit =  ui->infoWidget->findChild<QLineEdit*>("m_cityEdit");
+    m_cityPair = make_pair(m_cityLabel, m_cityEdit);
+    //  - NAME
+    QLabel* m_nameLabel = ui->infoWidget->findChild<QLabel*>("m_nameLabel");
+    QLineEdit* m_nameEdit =  ui->infoWidget->findChild<QLineEdit*>("m_nameEdit");
+    m_namePair = make_pair(m_nameLabel, m_nameEdit);
+    //  - PHONE
+    QLabel* m_phoneLabel = ui->infoWidget->findChild<QLabel*>("m_phoneLabel");
+    QLineEdit* m_phoneEdit =  ui->infoWidget->findChild<QLineEdit*>("m_phoneEdit");
+    m_phonePair = make_pair(m_phoneLabel, m_phoneEdit);
+    //  - STATE
+    QLabel* m_stateLabel = ui->infoWidget->findChild<QLabel*>("m_stateLabel");
+    QLineEdit* m_stateEdit =  ui->infoWidget->findChild<QLineEdit*>("m_stateEdit");
+    m_statePair = make_pair(m_stateLabel, m_stateEdit);
+    //  - ZIP
+    QLabel* m_zipLabel = ui->infoWidget->findChild<QLabel*>("m_zipLabel");
+    QLineEdit* m_zipEdit =  ui->infoWidget->findChild<QLineEdit*>("m_zipEdit");
+    m_zipPair = make_pair(m_zipLabel, m_zipEdit);
+
+    // Przyciski
+    m_okButton = ui->buttonsWidget->findChild<QPushButton*>("m_okButton");
+    m_cancelButton = ui->buttonsWidget->findChild<QPushButton*>("m_cancelButton");
+
 }
 
 AddressWindow::~AddressWindow()
--- 14/6/1/addresswindow.h
+++ 14/6/1/addresswindow.h
@@ -2,6 +2,11 @@
 #define ADDRESSWINDOW_H
 
 #include <QMainWindow>
+#include <QLabel>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+
 
 namespace Ui {
 class AddressWindow;
@@ -18,6 +23,21 @@ public:
 private:
     Ui::AddressWindow *ui;
 
+    // Pole dotyczace kraju jest unikalne
+    QPair<QLabel*, QComboBox*> m_countryPair;
+
+    //Wygodny typ pary laczacy QLabel i QLineEdit
+    using QInputPair_t = QPair<QLabel*, QLineEdit*>;
+
+    QInputPair_t m_addressPair;
+    QInputPair_t m_cityPair;
+    QInputPair_t m_namePair;
+    QInputPair_t m_phonePair;
+    QInputPair_t m_statePair;
+    QInputPair_t m_zipPair;
+
+    QPushButton* m_okButton;
+    QPushButton* m_cancelButton;
 };
 
 #endif // ADDRESSWINDOW_H
