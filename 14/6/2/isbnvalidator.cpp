0d9d7a3d80799b31ce678131429dc4751dd5d91a
Jurikimurik <makovskyiyurii@gmail.com>
2023-05-08 17:13:02 +0200

    14.6.2 is now ready.

--- 14/6/2/isbnvalidator.cpp
+++ 14/6/2/isbnvalidator.cpp
@@ -5,13 +5,34 @@ QValidator::State ISBNValidator::validate(QString &str, int &from) const
     QString strCopy = str;
     //Usuwamy wszystkie znaki, ktore nie sa liczbami
     strCopy.removeIf( [] (const QChar& ch) {
-        if(!ch.isDigit())
+        if(!ch.isDigit() && ch != 'X')
             return true;
         else
             return false;
     });
 
-    return Acceptable;
+    //Nie mamy wystarczajacej ilosci informacji
+    if(strCopy.size() < 10)
+        return Intermediate;
+    else if(strCopy.size() > 10)
+        return Invalid;
+
+    //Sprawdzamy kod ISBN 10-cyfrowy
+    int suma = 0; int length = strCopy.size();
+    for(int c = length, index = 0; c > 0; --c, ++index)
+    {
+        int liczbaKontrolna = c*strCopy.at(index).digitValue();
+        if(liczbaKontrolna < 1)
+            suma += c*10;
+        else
+            suma += liczbaKontrolna;
+    }
+
+
+    if(suma % (length+1) == 0)
+        return Acceptable;
+    else
+        return Intermediate;
 }
 
 ISBNValidator::ISBNValidator(QObject *parent) : QValidator(parent)
