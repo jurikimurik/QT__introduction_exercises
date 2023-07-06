6ae4c2bd3835be52e31133cf23b3ae48d5dfab8a
Jurikimurik <makovskyiyurii@gmail.com>
2023-07-06 14:49:21 +0200

    17.3.3 Image loading has been created.

--- 17/3/3/randomcollagecreator.cpp
+++ 17/3/3/randomcollagecreator.cpp
@@ -1,5 +1,8 @@
 #include "randomcollagecreator.h"
 
+#include <QDebug>
+#include <QDir>
+
 RandomCollageCreator::RandomCollageCreator(int width, int height, const QString &inputFolder, const QString &outputFolder) : m_width(width),
     m_height(height),
     m_inputFolder(inputFolder),
@@ -8,5 +11,48 @@ RandomCollageCreator::RandomCollageCreator(int width, int height, const QString 
 
 void RandomCollageCreator::generateImage()
 {
+    m_isReady = false;
+    loadImages();
+    transformImagesRandomly();
+    createOutputImage();
+    saveToFile();
+}
 
+void RandomCollageCreator::loadImages()
+{
+    QDir directory(m_inputFolder);
+    directory.setFilter(QDir::Files);
+    QFileInfoList list = directory.entryInfoList();
+    QVector<QString> filesPathes;
+    for(const QFileInfo &info : list)
+    {
+        if(IMAGE_FORMATS.contains(info.suffix().toLower()))
+            filesPathes << info.absoluteFilePath();
+    }
+
+    for(const QString& filePath : filesPathes)
+    {
+        QImage image(filePath);
+        m_images << image;
+    }
+}
+
+void RandomCollageCreator::transformImagesRandomly()
+{
+
+}
+
+void RandomCollageCreator::createOutputImage()
+{
+
+}
+
+void RandomCollageCreator::saveToFile()
+{
+    m_isReady = true;
+}
+
+bool RandomCollageCreator::isReady() const
+{
+    return m_isReady;
 }
--- 17/3/3/randomcollagecreator.h
+++ 17/3/3/randomcollagecreator.h
@@ -1,20 +1,36 @@
 #ifndef RANDOMCOLLAGECREATOR_H
 #define RANDOMCOLLAGECREATOR_H
 
+#include <QImage>
 #include <QString>
 
-
+const QStringList IMAGE_FORMATS = {
+  "png", "jpg", "jpeg"
+};
 
 class RandomCollageCreator
 {
 public:
     RandomCollageCreator(int width, int height, const QString &inputFolder, const QString &outputFolder);
     void generateImage();
+
+    bool isReady() const;
+
+private:
+    void loadImages();
+    void transformImagesRandomly();
+    void createOutputImage();
+    void saveToFile();
 private:
     int m_width;
     int m_height;
     QString m_inputFolder;
     QString m_outputFolder;
+
+    QVector<QImage> m_images;
+    QImage m_finalOutput;
+
+    bool m_isReady;
 };
 
 #endif // RANDOMCOLLAGECREATOR_H
